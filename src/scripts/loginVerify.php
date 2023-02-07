9850d46b98de35ff4b4d323dae8544735a0a42ea
Evans-GitHub <111977383+Evans-GitHub@users.noreply.github.com>
2023-02-07 13:55:57 +1030

    testing login verify

--- src/scripts/loginVerify.php
+++ src/scripts/loginVerify.php
@@ -1,13 +1,17 @@
 <?php
+session_start();
 
 if (isset($_POST["usernameInput"])){
+    
 
 $defaultUsername = "admin";
 $defaultPassword = "admin";
 
 $username = $_POST["usernameInput"];
 $password = $_POST["passwordInput"];
-if ($username = $defaultUsername && $password = $defaultPassword){
+// if ($username = $defaultUsername && $password = $defaultPassword){
+    if ($username = $defaultUsername){    
+    $_SESSION["username"] = $username;
     header("location: ../light-control.php");
 }
 else{
