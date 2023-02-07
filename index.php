5b44bbc4da2ce90e513e0e9e38d29d8ffbfc871d
Evans-GitHub <111977383+Evans-GitHub@users.noreply.github.com>
2023-02-07 12:32:36 +1030

    created lognin js, updated login/index pages

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="author" contents="regform">
+    <meta name="description" contents="index">
+    <title>Login</title>
+    <!-- <link rel="stylesheet" href="../output.css"> -->
+    <!-- <link rel="stylesheet" href="temp.css"> -->
+</head>
+<body>
+<h1></h1>
+
+<form action=playerVerify.php method="POST">
+<ul>
+    <li>
+    Enter your cool hacker name: <input type="text" class="inputBox" id="usernameInput" name="usernameInput" placeholder="Required"required autocomplete="off">
+    </li>
+    <li>
+    <input type="submit" class="listSubmit" id="startButton" name="startInput"
+    value="Start"></button>
+    </li>
+
+</form>
+</body>
+</html>
