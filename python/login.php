43f09573f067ba01d4ae7be0324c3499c7e7aabf
Evans-GitHub <111977383+Evans-GitHub@users.noreply.github.com>
2023-02-07 12:15:47 +1030

    added pages for index login and verification
    added base for index, login, and login verify

---
+++ python/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="author" contents="regform">
+    <meta name="description" contents="login">
+    <title>Login</title>
+    <!-- <link rel="stylesheet" href="../output.css"> -->
+    <!-- <link rel="stylesheet" href="temp.css"> -->
+</head>
+<body>
+<h1>Log In</h1>
+
+<form action=loginVerify.php method="POST">
+<ul>
+    <li>
+    Username: <input type="text" class="inputBox" id="usernameInput" name="usernameInput" placeholder="Required"required autocomplete="off">
+    </li>
+    <li>
+    Password: <input type="password" class="inputBox" id="passwordInput" name="passwordInput" placeholder="Required"required autocomplete="off">    
+    </li>
+    <li>
+    <input type="submit" class="listSubmit" id="loginButton" name="loginInput"
+    value="Log In"></button>
+    </li>
+
+</form>
+</body>
+</html>
