7895330c50843d942def719a08909b928d13630c
Evans-GitHub <111977383+Evans-GitHub@users.noreply.github.com>
2023-02-07 13:44:49 +1030

    login should now check if the username/password match  the default, then redirect appropriately
    also removed login.js because we dont need it

--- login/loginVerify.php
+++ login/loginVerify.php
@@ -1,15 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="utf-8">
-    <meta name="author" contents="regform">
-    <meta name="description" contents="login">
-    <title>Login</title>
-    <!-- <link rel="stylesheet" href="../output.css"> -->
-    <!-- <link rel="stylesheet" href="temp.css"> -->
-</head>
-<body>
-    hey
-</body>
-</html>
+if (isset($_POST["usernameInput"])){
+
+$defaultUsername = 'admin';
+$defaultPassword = 'admin';
+
+$username = $_POST["usernameInput"];
+$password = $_POST["passwordInput"];
+if ($username = $defaultUsername && $password = $defaultPassword){
+    header("location: light-control.php");
+}
+else{
+    header("location: login.php");
+}
+
+}
+
+
+
+
+?>
--- src/index.php
+++ src/index.php
@@ -40,14 +40,14 @@
 -->
 <div class="bg-white shadow sm:rounded-lg">
   <div class="px-4 py-5 sm:p-6">
-    <h3 class="text-lg font-medium leading-6 text-gray-900">Update your email</h3>
+    <h3 class="text-lg font-medium leading-6 text-gray-900">Enter your cool hacker name</h3>
     <div class="mt-2 max-w-xl text-sm text-gray-500">
       <p>Change the email address you want associated with your account.</p>
     </div>
     <form class="mt-5 sm:flex sm:items-center">
       <div class="w-full sm:max-w-xs">
         <label for="email" class="sr-only">Email</label>
-        <input type="email" name="email" id="email" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="you@example.com">
+        <input type="email" name="email" id="email" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="required" autocomplete="off">
       </div>
       <button type="submit" class="mt-3 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Save</button>
     </form>
