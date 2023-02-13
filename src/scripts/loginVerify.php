a0256f092cdb3240241864d229dbf822f71473a4
Evans-GitHub <111977383+Evans-GitHub@users.noreply.github.com>
2023-02-13 11:52:21 +1030

    scoreboard test stuff

--- src/scripts/loginVerify.php
+++ src/scripts/loginVerify.php
@@ -3,6 +3,9 @@ session_start();
 
 if (isset($_POST["usernameInput"])){
     
+ require_once "dbconn.php";
+
+  
 
 $defaultUsername = "admin";
 $defaultPassword = "admin";
@@ -25,6 +28,35 @@ $password = $_POST["passwordInput"];
     // echo $_POST["usernameInput"];
     // echo $_POST["passwordInput"];
    
+    // UPDATE Customers
+    // SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+    // WHERE CustomerID = 1;
+
+    // $sql = "UPDATE players
+    // SET task1 = '1'
+    // WHERE user_id = ;
+    // $statement = mysqli_stmt_init($conn);
+    // mysqli_stmt_prepare($statement, $sql);
+    // mysqli_stmt_bind_param($statement, 's', htmlspecialchars($_POST["playerName"]));
+    // $result = mysqli_query($conn, $sql);
+    // $row = mysqli_fetch_assoc($result);
+
+
+    // if (mysqli_stmt_execute($statement)) {
+    //     // $success = "Account created! Click here to login";
+    //     header("location: ../light-control.php");
+    //     end();
+    // }
+    // else {
+    //     header("location: ../index");
+    //     echo mysqli_error($conn);
+    //     end();
+    // }
+    // mysqli_close($conn);
+   
+//the above code is an attempt to add a check that marks off task 1, it requires a session variable to exist that 
+//can be used by an sql statement to id the user
+
     header("location: ../light-control.php");
 }
 else{
--- src/scripts/playerVerify.php
+++ src/scripts/playerVerify.php
@@ -1,24 +1,53 @@
+session_start();
 <?php
 echo 'please go back and enter your name';
 
+function guidv4($data = null) {
+    // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+    $data = $data ?? random_bytes(16);
+    assert(strlen($data) == 16);
+
+    // Set version to 0100
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+    // Set bits 6-7 to 10
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+    // Output the 36 character UUID.
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
+
 if (isset($_POST["playerName"])){
     require_once "dbconn.php";
-
-    $sql = "INSERT INTO players(username) VALUES(?);";
+    $myuuid = guidv4();
+    $sql = "INSERT INTO players(username, uuid) VALUES(?,?);";
     $statement = mysqli_stmt_init($conn);
     mysqli_stmt_prepare($statement, $sql);
-    mysqli_stmt_bind_param($statement, 's', htmlspecialchars($_POST["playerName"]));
+    mysqli_stmt_bind_param($statement, 'ss', htmlspecialchars($_POST["playerName"]), $myuuid);
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result);
 
 
     if (mysqli_stmt_execute($statement)) {
         // $success = "Account created! Click here to login";
+
+        // $sql = "SELECT user_id FROM players WHERE VALUES(?);";
+        // $statement = mysqli_stmt_init($conn);
+        // mysqli_stmt_prepare($statement, $sql);
+        // //mysqli_stmt_bind_param($statement, 's', htmlspecialchars($_POST["playerName"]));
+        // $result = mysqli_query($conn, $sql);
+        // $row = mysqli_fetch_assoc($result);
+
+
+        // $_SESSION['user_id'] = 
+        
+        //above code is an attempt to set a unique session variable so we can check off tasks
+        
+        $_SESSION['tasksCompleted'] = "0";
         header("location: ../login.php?loggedin=false");
         end();
     }
     else {
-        header("location: ../index");
+        header("location: ../index.php");
         echo mysqli_error($conn);
         end();
     }
